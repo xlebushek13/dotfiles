884941ff1a57faadb4349fbe041b21129ed78307
xlebushek13 <xlebushekbb@gmail.com>
2024-09-22 01:38:29 +0300

    Fixes

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
-echo 'Completed install Oh My Zsh' && cp .zshrc .zshistory .zsh_history ~/;
+echo 'Completed install Oh My Zsh' && cp .zshrc .zhistory .zsh_history ~/;
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
